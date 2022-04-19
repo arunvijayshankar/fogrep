87ef538353f9a47a626c3358b49ab890739e7634
Arun Vijayshankar <arunvijayshankar@gmail.com>
2022-04-19 15:42:57 -0700

    reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect(); // init vector to hold cmd line args
 
+    // store relavant args in variables
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for `{}`", query);
+    println!("In file `{}`", filename);
+
+    // store file contents in variable using fs function
+    let contents = fs::read_to_string(filename)
+        .expect("Error in reading file");
+
+    println!("With text:\n{}", contents);
 }
