0f2ac1c637e2a43bb5925124ead46c9e147c5a37
Arun Vijayshankar <arunvijayshankar@gmail.com>
2022-04-20 22:49:32 -0700

    something weird

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
-
-// stores config variables
-struct Config {
-    query: String,
-    filename: String,
-}
-
-// parsing function implemented on Config
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-// logic
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
+use fogrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // init vector to hold cmd line args
@@ -43,7 +14,7 @@ fn main() {
     println!("Searching for `{}`", config.query);
     println!("In file `{}`", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = fogrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
