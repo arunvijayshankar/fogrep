9a5c37153d67f1d31889c38eda048d7e80fe6b87
Arun Vijayshankar <arunvijayshankar@gmail.com>
2022-04-20 22:43:12 -0700

    created lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+
+// stores config variables
+struct Config {
+    query: String,
+    filename: String,
+}
+
+// parsing function implemented on Config
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+// logic
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // init vector to hold cmd line args
 
-    // store relavant args in variables
-    let query = &args[1];
-    let filename = &args[2];
+    // function to parse cmd line args for config variables
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for `{}`", query);
-    println!("In file `{}`", filename);
+    println!("Searching for `{}`", config.query);
+    println!("In file `{}`", config.filename);
 
-    // store file contents in variable using fs function
-    let contents = fs::read_to_string(filename)
-        .expect("Error in reading file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
